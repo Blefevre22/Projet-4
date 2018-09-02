a15dc068bda5768ff5d05bb511a08cafce641e67
Blefevre22 <benoit.lefevre22@gmail.com>
2018-09-02 22:59:01 +0200

    sauvegarde du 02-09-2018

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -15,7 +15,6 @@ use Doctrine\ORM\EntityManagerInterface;
 class CustomerType extends AbstractType
 {
     private $entityManager;
-
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
