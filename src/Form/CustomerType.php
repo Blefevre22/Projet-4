83a5459c17610e22884c0cdfeb73e7a8d6d13960
Blefevre22 <benoit.lefevre22@gmail.com>
2018-09-02 22:49:06 +0200

    datepicker ok

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -15,7 +15,7 @@ use Doctrine\ORM\EntityManagerInterface;
 class CustomerType extends AbstractType
 {
     private $entityManager;
-
+toto
     public function __construct(EntityManagerInterface $entityManager)
     {
         $this->entityManager = $entityManager;
