4e8381fda18983b8d6978cbcb24e9cdc2e88de86
Blefevre22 <benoit.lefevre22@gmail.com>
2018-09-30 13:40:54 +0200

    Gestion selection du ticket : ok

--- public/js/index.js
+++ public/js/index.js
@@ -1,22 +1,38 @@
 $(document).ready(function() {
     //Vérification des jours à plus de 1000 réservations
-    $( '#booking' ).on( "mousedown", ".tinymce", function() {
+    $('#booking').on("mousedown", ".date", function () {
         datePicker('{{ disableDate|json_encode() }}');
     });
-    $( '#booking' ).on( "change", ".tinymce", function() {
+    $('#booking').on("change", ".date", function () {
         var birthDate = this.id,
-            birth = $('#'+birthDate),
+            birth = $('#' + birthDate),
             idParent = $(this).parent().parent().attr('id');
         $.ajax({
             url: 'jquery-price',
             method: 'GET',
-            data: "date="+birth.val(),
+            data: "date=" + birth.val(),
             success: function (data) {
-                if($('#'+idParent+' .tarif').length){
-                    $('#'+idParent+' .tarif').remove();
+                if ($('#' + idParent + ' .tarif').length) {
+                    $('#' + idParent + ' .tarif').remove();
                 }
-                $('#'+idParent+' .checkbox').after("<p class='tarif'>Tarif: "+data.data+'€</p>');
+                $('#' + idParent + ' .checkbox').after("<p class='tarif'>Tarif: " + data.data + '€</p>');
             }
         })
     });
-});
+    $('#booking').on("change", ".datepickerBooking", function () {
+        var registration_date = $('#booking_registrationDate');
+        var halfDay = $('.radio').eq(0);
+        $.ajax({
+            url: 'jquery-ticket',
+            method: 'GET',
+            data: "date=" + registration_date.val(),
+            success: function (data) {
+                if (data['data'] === true) {
+                    halfDay.hide();
+                }else{
+                    halfDay.show();
+                }
+            }
+        });
+    });
+})
--- src/Controller/PriceController.php
+++ src/Controller/PriceController.php
@@ -19,4 +19,25 @@ class PriceController extends Controller
         return new JsonResponse(array('data' => $data));
     }
 
+    /**
+     * @Route("/jquery-ticket", name="jquery-ticket")
+     */
+    public function jqueryTicket(Request $request)
+    {
+        $data = $request->query->get('date');
+        $data = str_replace('/', '-', $data);
+        $registration = new \DateTime($data);
+        $today = new \DateTime();
+        if($today->format('d-m-y') === $registration->format('d-m-y')){
+            if($today->format('H') > '14'){
+                $response = true;
+            }else{
+                $response = false;
+            }
+        }else{
+            $response = false;
+        }
+        return new JsonResponse(array('data' => $response));
+    }
+
 }
